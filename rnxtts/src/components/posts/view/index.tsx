96c15d46a637a1623106482fe31733d0304cbaef
DP <denniepe.adk@gmail.com>
2023-06-19 22:50:07 +0800

    font sizing

--- rnxtts/src/components/posts/view/index.tsx
+++ rnxtts/src/components/posts/view/index.tsx
@@ -27,7 +27,7 @@ const PostView = (props: PostWithUser) => {
                         {dayjs(post.createdAt).fromNow()}
                     </span>
                 </div>
-                <span>{post.content}</span>
+                <span className="text-2xl">{post.content}</span>
             </div>
         </div>
     )
